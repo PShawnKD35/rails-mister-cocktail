f1531e13da2ebb07173ce1f071fba1cf4a92eb73
Shawn Peng <shawn-peng@outlook.com>
2019-07-25 18:37:43 +0800

    Added everything in controller and view

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
@@ -5,11 +7,28 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @ingredients_names = Ingredient.all.select(&:name)
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(params.require(:cocktail).permit(:name))
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if Cocktail.delete(params[:id])
+      redirect_to cocktails_path
+    else
+      render text: 'Deleting failed for no reason!'
+    end
   end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,24 @@
 class DosesController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @params = params.require(:dose).permit(:description, :ingredient_id)
+    # @dose = Dose.new(description: @params[:description])
+    @dose = Dose.new(@params)
+    # @dose.ingredient = Ingredient.find_by(name: @params[:ingredient])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    if Dose.delete(params[:id])
+      redirect_to cocktail_path(@dose.cocktail_id)
+    else
+      render text: "Deleting failed for no reason!!!"
+    end
+  end
 end
