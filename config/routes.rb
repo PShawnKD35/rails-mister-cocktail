11863068ef35e5758814a5658d3e1fbe39a873f6
Shawn Peng <shawn-peng@outlook.com>
2019-07-25 16:18:36 +0800

    Added controllers and routes and an index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :coctails, only: [:index, :show, :new, :delete] do
+    resources :doses, only: [:new, :delete]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
